87fd32af91d29f7cf0c74dae7671b74b31273f89
Daniel Warrick <danielwarrick@Daniels-MacBook-Pro.local>
2024-09-23 16:34:43 -0400

    boilerplate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
